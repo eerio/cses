24e3a954328b5825c02d9e9d1352fa8726cdb7f4
Paweł Balawender <pawel.balawender@mim-solutions.pl>
2021-09-08 16:26:44 +0200

    start

---
+++ coin-combinations-ii.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define MOD (1000000007U)
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, x;
+  cin >> n >> x;
+  
+  int c[n];
+  for (int i=0; i < n; ++i) cin >> c[i];
+  sort(c, c + n);
+
+  unsigned int dp[x + 1] = {1};
+
+  for (int i=1; i <= x; ++i) {
+    unsigned int last = UINT_MAX;
+    for (int j=0; j < n; ++j) {
+      if (c[j] == last) continue;
+      else last = c[j];
+
+      dp[i] += i >= c[j] ? dp[i - c[j]] : 0;
+      dp[i] %= MOD;
+    }
+  }
+
+  cout << dp[x] << endl;
+
+  return 0;
+}
