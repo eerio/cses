19f74b6051f543b30836593bba62a0f0e3fa3201
Paweł Balawender <pawel.balawender@mim-solutions.pl>
2021-09-10 11:23:38 +0200

    update

--- boiler.cpp
+++ boiler.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
+#define MOD (1000000007U)
+#define FOR(a) (for (int i=0; i < a; ++i))
 using namespace std;
+
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
